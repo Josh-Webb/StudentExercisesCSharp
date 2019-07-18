8422b5a5601435bfe9887ca2fdfb066ca9d03b84
Josh Webb <joshwebb42@gmail.com>
2019-07-18 15:34:36 -0500

    implemented inheritance for students and instructors

---
+++ NSSPerson.cs
@@ -0,0 +1,13 @@
+namespace StudentExercises {
+    class NSSPerson{
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Slack { get; set; }
+
+    public Cohort Cohort { get; set; }
+
+    }
+}
--- Student.cs
+++ Student.cs
@@ -1,17 +1,10 @@
 using System;
 using System.Collections.Generic;
 
-namespace StudentExercises {
-    class Student
+namespace StudentExercises 
+{
+    class Student : NSSPerson
     {
-        public string FirstName { get; set; }
-
-        public string LastName { get; set; }
-
-        public string Slack { get; set; }
-
-        public Cohort Cohort { get; set; }
-
         public List<Exercise> Exercises  = new List<Exercise>();
 
     }
